8706c93be574fee0114a7584f7a9b2432ae9806b
Rohan Tiwari <rohanitsector@gmail.com>
2025-11-09 15:57:24 +0530

    Added Album Display and its navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
                  <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>R</p>
              </div>
          </div>
-         <div className='flex items-center gap-2 mt-4'>
+         <div className='flex items-center gap-2 mt-2'>
              <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'> All</p>
              <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
              <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
