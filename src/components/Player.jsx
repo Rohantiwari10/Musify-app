1c3260582b6e16bda8c00df3443b8278fda9e910
Rohan Tiwari <rohanitsector@gmail.com>
2025-11-10 12:18:51 +0530

    SeekBar Functionality

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -6,7 +6,7 @@ import { PlayerContext } from "../context/PlayerContext";
 
 const Player = () => {
 
-  const {track, seekBar, seekBg,playStatus, play, pause, time,previous, next} = useContext(PlayerContext);
+  const {track, seekBar, seekBg,playStatus, play, pause, time,previous, next,seekSong} = useContext(PlayerContext);
 
   return (
     <div className="h-[10%] bg-black flex justify-between items-center text-white px-4">
@@ -34,7 +34,7 @@ const Player = () => {
         </div>
         <div className="flex items-center gap-5">
             <p>{time.currentTime.minute}:{time.currentTime.second}</p>
-            <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+            <div ref={seekBg} onClick={seekSong} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
                 <hr ref={seekBar} className="h-1 brder-none w-0 bg-green-800 rounded-full" />
             </div>
             <p>{time.totalTime.minute}:{time.totalTime.second}</p>
